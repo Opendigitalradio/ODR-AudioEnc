cd93ca37c7a9688e1a1273d62623b2b9e17b36ca
Matthias P. Braendli <matthias.braendli@mpb.li>
2025-08-31 19:01:36 +0200

    Remove trailing spaces

--- contrib/edioutput/Transport.cpp
+++ contrib/edioutput/Transport.cpp
@@ -210,8 +210,8 @@ void Sender::tcp_send_client_t::send_packet(const std::vector<uint8_t> &frame)
     const auto error_stats = sock.sendall(frame);
 
     if (verbose and error_stats.has_seen_new_errors) {
-        etiLog.level(warn) << "TCP output " << dest_addr << ":" << dest_port 
-                          << " has " << error_stats.num_reconnects 
+        etiLog.level(warn) << "TCP output " << dest_addr << ":" << dest_port
+                          << " has " << error_stats.num_reconnects
                           << " reconnects: most recent error: " << error_stats.last_error;
     }
 }
